386782d55358daed5fca2b5465213a401391b2d5
Pooja Paul <71924871+Pooja113@users.noreply.github.com>
2022-05-10 00:01:33 +0530

    Updation

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -24,18 +24,9 @@ export const createPost = async (req,res) => {
 }
 
 export const updatePost = async (req,res) => {
-
   const {id : _id} = req.params;
   const post = req.body;
   if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send("No Post for this ID") 
-
   const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, {new:true})
   res.json(updatedPost)
-  // try {
-  //   await newPost.save();
-  //   res.status(201).json(newPost)
-  // } catch (error) {
-  //   res.status(409).json({error: error.message})
-
-  // }
 }
