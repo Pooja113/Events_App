dde29f8c7bb795982d6176512aebd24eaad749b1
Pooja Paul <71924871+Pooja113@users.noreply.github.com>
2022-05-10 09:56:07 +0530

    Update, Delete and LikePost functonality

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -27,6 +27,21 @@ export const updatePost = async (req,res) => {
   const {id : _id} = req.params;
   const post = req.body;
   if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send("No Post for this ID") 
-  const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, {new:true})
+  const updatedPost = await PostMessage.findByIdAndUpdate(_id, {...post,_id}, {new:true})
   res.json(updatedPost)
+}
+
+export const deletePost = async (req,res) => {
+  const {id} = req.params;
+  if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send("No Post for this ID") 
+  await PostMessage.findByIdAndRemove(id);
+  res.json({message:"Post Deleted Successfully"})
+}
+
+export const likePost = async (req,res) => {
+  const {id} = req.params;
+  if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send("No Post for this ID") 
+  const post = await PostMessage.findById(id);
+  const updatePost = await PostMessage.findByIdAndUpdate(id, {likeCount: post.likeCount+1}, {new:true})
+  res.json(updatePost)
 }
--- server/index.js
+++ server/index.js
@@ -9,12 +9,12 @@ const app = express();
 
 app.use(bodyParser.json({ limit: "30mb" , extended : true}));
 app.use(bodyParser.urlencoded({ limit: "30mb" , extended : true}));
-const corsOptions ={
-    origin:'http://localhost:3000', 
-    credentials:true,       
-    optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
+// const corsOptions ={
+//     origin:'http://localhost:3000', 
+//     credentials:true,       
+//     optionSuccessStatus:200
+// }
+app.use(cors());
 
 
 
