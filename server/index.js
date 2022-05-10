33bc4444bf45d6db02aa25ecb9d59f3ffc3a2fc9
Pooja Paul <71924871+Pooja113@users.noreply.github.com>
2022-05-10 12:56:26 +0530

    All the basic functionalities

--- server/index.js
+++ server/index.js
@@ -3,27 +3,21 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from 'cors';
 import postRoutes from './routes/posts.js'
-
+import dotenv from 'dotenv'
 
 const app = express();
+dotenv.config()
 
 app.use(bodyParser.json({ limit: "30mb" , extended : true}));
 app.use(bodyParser.urlencoded({ limit: "30mb" , extended : true}));
-// const corsOptions ={
-//     origin:'http://localhost:3000', 
-//     credentials:true,       
-//     optionSuccessStatus:200
-// }
 app.use(cors());
 
 
 
 app.use('/posts',postRoutes)
-
-const DBCONNECTION_URL = 'mongodb+srv://site_admin:ud8CB25JsSpG9Zjb@cluster0.bp7fo.mongodb.net/eventsDB?retryWrites=true&w=majority'
-const PORT = process.env.PORT || 5000;
-
-mongoose.connect(DBCONNECTION_URL , { useNewUrlParser: true, useUnifiedTopology:true})
+const DBCONNECTION_URI = process.env.DBCONNECTION_URI
+const PORT = process.env.PORT;
+mongoose.connect(DBCONNECTION_URI , { useNewUrlParser: true, useUnifiedTopology:true})
 .then(()=>app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`)))
 .catch((error)=> console.log(error.message));
 
