fbe63f1b25ce9060234aa518b28d232163db9702
Pooja Paul <71924871+Pooja113@users.noreply.github.com>
2022-06-22 19:45:35 +0530

    Deploy

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ import userRoutes from './routes/user.js'
 import dotenv from 'dotenv'
 
 const app = express();
-dotenv.config({path: "config.env"})
+dotenv.config({path: "server/config.env"})
 
 app.use(bodyParser.json({ limit: "30mb" , extended : true}));
 app.use(bodyParser.urlencoded({ limit: "30mb" , extended : true}));
