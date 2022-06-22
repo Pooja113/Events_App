201538b140d03bf9b76af2026ca16e295ff917f6
Pooja Paul <71924871+Pooja113@users.noreply.github.com>
2022-06-22 21:39:38 +0530

    Bug Fix

---
+++ client/build/precache-manifest.20216bfc7d0b8078d771141b866fddf2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a78b602e1b73d18750b577efada0c646",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84593a1cf5791a83d00d",
+    "url": "/static/css/main.32a5d357.chunk.css"
+  },
+  {
+    "revision": "a3fa1145ddc17e93ed69",
+    "url": "/static/js/2.a21a3126.chunk.js"
+  },
+  {
+    "revision": "c06d497d6716e84111c2fd9253bcfc02",
+    "url": "/static/js/2.a21a3126.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84593a1cf5791a83d00d",
+    "url": "/static/js/main.2fc1c952.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "f3de7abc31aa85cee55d42c9656c25f7",
+    "url": "/static/media/event.f3de7abc.png"
+  }
+]);
